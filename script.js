336b199942bdb870815879536bca3d346f83d76e
tripleRRR33 <florianroy555@gmail.com>
2025-01-09 12:30:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function() {
+(function() {
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
