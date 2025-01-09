eeb68baad993e2f2007b10dec8aa19b65f3959ad
tripleRRR33 <florianroy555@gmail.com>
2025-01-09 12:21:37 +0100

    Create question.js

---
+++ question.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+        question: "Quelle est la capitale de la France?",
+        answers: {
+            a: "Berlin",
+            b: "Madrid",
+            c: "Paris",
+            d: "Lisbonne"
+        },
+        correctAnswer: "c"
+    },
+    // Ajoutez ici les 199 autres questions de culture générale
+];
